2d90655e9c4c323ba924edab53fff62738b22a82
Saravana Balaji <saravanabalajis49@gmail.com>
2021-07-11 21:51:52 +0530

    Interview feedback fix, Error handling and fallback UI

--- src/components/Templates/CandidateProfilePage/CandidateProfilePage.tsx
+++ src/components/Templates/CandidateProfilePage/CandidateProfilePage.tsx
@@ -1,38 +1,50 @@
 import { Flex, FlexItem, PageSection } from '@patternfly/react-core';
 import React from 'react';
 import { PageTitle } from '../../Atoms';
+import { ErrorBoundary } from 'react-error-boundary'
 import { CandidateProfileBasicInfo, CandidateProfileSummary, CandidateProfileExperience, CandidateProfileEducation, CandidateProfileResume } from '../../Organisms';
+import ErrorComponent from '../ErrorComponent/ErrorComponent';
 import './CandidateProfilePage.css';
 
-const CandidateProfilePage = () => {
+const CandidateProfilePage = (props) => {
+    const handleError = (): void => {
+        props.history.push('/profile');
+    }
     return (
         <React.Fragment>
-            <PageSection variant="light">
-                <PageTitle title='Profile' />
-            </PageSection>
-            <PageSection>
-                <Flex>
-                    <Flex direction={{ default: 'column'}}>
-                        <FlexItem>
-                            <CandidateProfileSummary />
-                        </FlexItem>
+            <ErrorBoundary
+                FallbackComponent={ErrorComponent}
+                onReset={() => {
+                    handleError()
+                }}
+            >
+                <PageSection variant="light">
+                    <PageTitle title='Profile' />
+                </PageSection>
+                <PageSection>
+                    <Flex>
+                        <Flex direction={{ default: 'column' }}>
+                            <FlexItem>
+                                <CandidateProfileSummary />
+                            </FlexItem>
+                        </Flex>
+                        <Flex direction={{ default: 'column' }} alignSelf={{ default: 'alignSelfStretch' }} flex={{ default: 'flex_1' }}>
+                            <FlexItem>
+                                <CandidateProfileBasicInfo />
+                            </FlexItem>
+                            <FlexItem className="candidate-profile-flex-style">
+                                <CandidateProfileExperience />
+                            </FlexItem>
+                            <FlexItem>
+                                <CandidateProfileEducation />
+                            </FlexItem>
+                            <FlexItem className="candidate-profile-flex-style">
+                                <CandidateProfileResume />
+                            </FlexItem>
+                        </Flex>
                     </Flex>
-                    <Flex direction={{ default: 'column' }} alignSelf={{ default: 'alignSelfStretch' }} flex={{ default: 'flex_1' }}>
-                        <FlexItem>
-                            <CandidateProfileBasicInfo />
-                        </FlexItem>
-                        <FlexItem className="candidate-profile-flex-style">
-                            <CandidateProfileExperience />
-                        </FlexItem>
-                        <FlexItem>
-                            <CandidateProfileEducation />
-                        </FlexItem>
-                        <FlexItem className="candidate-profile-flex-style">
-                            <CandidateProfileResume />
-                        </FlexItem>
-                    </Flex>
-                </Flex>
-            </PageSection>
+                </PageSection>
+            </ErrorBoundary>
         </React.Fragment>
     );
 }
